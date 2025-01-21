77418dbec761a1a051245aa1e0405b4acd5e5ab6
ibarland <ibarland@radford.edu>
2025-01-20 21:21:54 -0500

    update

--- NearlyEqualsTest.java
+++ NearlyEqualsTest.java
@@ -2,12 +2,7 @@
 
 import java.util.*;
 
-class UtilsIan {
-  //----------------------------------------------------------------
-  // static helper functions:
-  // Mostly, type-conversion functions with names I can remember.
-  // (Include checking for null and good error message, for CS1 students.)
-  
+class NearlyEqualsTest {
   /*
   int debugOutThreshold = 0;
   // methods assert, debugPrint
@@ -19,25 +14,6 @@ class UtilsIan {
   
   // todo: cast thiz,that using reflection.  -- no, not needed(?).
   static boolean equals( Object thiz, Object that ) { return thiz.equals(that); }
-  // todo: catch nullPointerExceptions
-  //public static boolean aString_equals( String a, String b ) { return a.equals(b); }
-  static String  intToString( int n ) { return Integer.toString(n); }
-  static String  doubleToString( double x ) { return Double.toString(x); }
-  static double  intToDouble( int n ) { return (double)n; }
-  static int     doubleToInt( double x ) { return (int)Math.round(x); }
-  static String  charToString( Character c ) {
-      verifyNonNull(c,"charToString","char or Character");
-      return Character.toString(c); }
-  static char    stringToChar( String s ) {
-    verifyNonNull(s, "stringToChar", "String");
-    if (s.length() != 1) throw new IllegalArgumentException( "stringToChar: Can only convert strings of length 1; got: \"" + s + "\"." );
-    return s.charAt(0);
-    }
-  
-  private static void verifyNonNull( Object obj, String funcName, String expectedType ) {
-    if (obj==null) throw new NullPointerException( funcName + ": Was passed null, instead of a " + expectedType + "."
-                                                   + "(Did you forget to initialize a variable?)" );
-    }
 
 
     static final int DEFAULT_BITS_TOLERANCE = 20; // very generous?; 5 is more resonable?
@@ -68,6 +44,11 @@ class UtilsIan {
      *  @license CC-BY
      */
     public static boolean equalsApprox( double a, double b, int bitsTolerance ) { 
+        return equalsApprox_v4(a,b,bitsTolerance);
+        }
+    public static boolean equalsApprox( double a, double b ) { return equalsApprox(a,b,DEFAULT_BITS_TOLERANCE); }
+
+    public static boolean equalsApprox_v1( double a, double b, int bitsTolerance ) { 
         return  a==b // hotpath; also handles infinities and ±0s (but not NaNs) ?
             || Math.abs(a-b)  <  (Math.min(Math.ulp(a), Math.ulp(b)) * (0b1L << bitsTolerance))
         /* other implementations, ranked by least-to-most failed tests (keep the a==b and NaN above, though): */
@@ -76,10 +57,52 @@ class UtilsIan {
             || (Double.isNaN(a) && Double.isNaN(b))
             ;
         }
-    public static boolean equalsApprox( double a, double b ) { return equalsApprox(a,b,DEFAULT_BITS_TOLERANCE); }
+    public static boolean equalsApprox_v1( double a, double b ) { return equalsApprox_v1(a,b,DEFAULT_BITS_TOLERANCE); }
+    public static boolean equalsApprox_v2( double a, double b, int bitsTolerance ) { 
+        return  a==b // hotpath; also handles infinities and ±0s (but not NaNs) ?
+        //   || Math.abs(a-b)  <  (Math.min(Math.ulp(a), Math.ulp(b)) * (0b1L << bitsTolerance))
+        /* other implementations, ranked by least-to-most failed tests (keep the a==b and NaN above, though): */
+            || Math.abs(Double.doubleToLongBits(a) - Double.doubleToLongBits(b)) < (0b1L << bitsTolerance)
+        //  || Math.abs(Double.doubleToLongBits(a-b)) < (0b1L << bitsTolerance)
+            || (Double.isNaN(a) && Double.isNaN(b))
+            ;
+        }
+    public static boolean equalsApprox_v2( double a, double b ) { return equalsApprox_v2(a,b,DEFAULT_BITS_TOLERANCE); }
+    public static boolean equalsApprox_v3( double a, double b, int bitsTolerance ) { 
+        return  a==b // hotpath; also handles infinities and ±0s (but not NaNs) ?
+        //  || Math.abs(a-b)  <  (Math.min(Math.ulp(a), Math.ulp(b)) * (0b1L << bitsTolerance))
+        /* other implementations, ranked by least-to-most failed tests (keep the a==b and NaN above, though): */
+        //  || Math.abs(Double.doubleToLongBits(a) - Double.doubleToLongBits(b)) < (0b1L << bitsTolerance)
+            || Math.abs(Double.doubleToLongBits(a-b)) < (0b1L << bitsTolerance)
+            || (Double.isNaN(a) && Double.isNaN(b))
+            ;
+        }
+    public static boolean equalsApprox_v3( double a, double b ) { return equalsApprox_v3(a,b,DEFAULT_BITS_TOLERANCE); }
+    public static boolean equalsApprox_v4( double a, double b, int bitsTolerance ) { 
+        if (a==b || (Double.isNaN(a) && Double.isNaN(b))) return true;
+        if (Math.signum(a)==Math.signum(b)) {
+            long MASK = Long.MAX_VALUE << bitsTolerance;
+            return (Double.doubleToLongBits(a)&MASK) == (Double.doubleToLongBits(b)&MASK);
+            }
+        else { // opposite signs: they're only close if both are 0, up to bitsTolerance-1
+            long MASK = Long.MAX_VALUE << (bitsTolerance-1);
+            return (Double.doubleToLongBits(Math.abs(a))&MASK) == 0
+                && (Double.doubleToLongBits(Math.abs(b))&MASK) == 0;
+            }
+        }
+    public static boolean equalsApprox_v4( double a, double b ) { return equalsApprox_v4(a,b,DEFAULT_BITS_TOLERANCE); }
     
     // @Test
     public static void testEqualsApprox() {
+        log("A few basic tests\n");
+        assertTrue(  equalsApprox( 1.23, 1.23, 0 ));
+        assertTrue(  equalsApprox( 1.23, 1.23, 1 ));
+        assertTrue(  equalsApprox( 1.23, 1.23, 25 ));
+        assertFalse( equalsApprox( 1.23, 1.24, 25 ));
+        assertTrue(  equalsApprox( 1.23, 1.23000001, 32 ));
+        assertTrue(  equalsApprox( 1.23, 1.23000011, 32 ));
+        assertFalse( equalsApprox( 1.23, 1.23000111, 32 ));
+
         double x1 = 3.0;
         double x2 = 3.000_000_000_000_001; // within ~1e15 -- close to max precision
         double y  = x1 * 1.4;
@@ -87,6 +110,7 @@ class UtilsIan {
         double pos0 = 1.0/Double.POSITIVE_INFINITY;
         double neg0 = 1.0/Double.NEGATIVE_INFINITY;
 
+        log("Check various scales\n");
         /* DISCLAIMER: this testing is not as thorough as it should be, but I'm too lazy to
          * think about IEEE double format to look for odd corner-cases.
          */
@@ -100,22 +124,32 @@ class UtilsIan {
             testEqualsApprox2( x2*scale, z *scale, false );
             testEqualsApprox2( y *scale, z *scale, false );
             }
+
+        log("pos/neg zeroes, and near-zeroes\n");
         assertTrue(  equalsApprox(  0.0,  0.0, 0 ));
         assertTrue(  equalsApprox(  0.0, neg0, 0 ));
         assertTrue(  equalsApprox( pos0,  0.0, 0 ));
         assertTrue(  equalsApprox( pos0, neg0, 0 ));
+
         assertTrue(  equalsApprox( +Double.MIN_VALUE, 0, 1 ));
         assertTrue(  equalsApprox( -Double.MIN_VALUE, 0, 1 ));
-        assertFalse( equalsApprox( +Double.MIN_VALUE, -Double.MIN_VALUE, 0 ));   // FAILS v1
-        assertFalse( equalsApprox( +Double.MIN_VALUE*2, -Double.MIN_VALUE*2, 2 ));   // FAILS v1
+        assertFalse( equalsApprox( +Double.MIN_VALUE  , -Double.MIN_VALUE  , 0 ));   // FAILS v1
+        assertTrue(  equalsApprox( +Double.MIN_VALUE  , -Double.MIN_VALUE  , 1 ));   // FAILS v1
+        assertTrue( equalsApprox( +Double.MIN_VALUE*2, -Double.MIN_VALUE*2, 2 ));   // FAILS v1
         assertTrue(  equalsApprox( +Double.MIN_VALUE*2, -Double.MIN_VALUE*2, 3 ));
+
+        assertTrue(  equalsApprox(0.0,  1e-320, 11) ); // passes
+        assertTrue(  equalsApprox(0.0,  1e-321, 10) ); // passes
         
+
+        log("using default precision\n");
         assertTrue(  equalsApprox(7.0,  7.0/25.0*25.0) );
         assertFalse( equalsApprox(0.0,  7.0 - (7.0/25.0*25.0)) );  // SURPRISING compared to previous?
         assertFalse( equalsApprox(0.0,  1e-17) );                  // FAILS v1-v3
         assertFalse( equalsApprox(0.0,  1e-87) );                  // FAILS v1-v3
         assertFalse( equalsApprox(0.0,  1e-307) );                 // FAILS v1-v3
 
+        log("using default precision, near 1.0\n");
         assertTrue(  equalsApprox(1.000000000002e-17,  1e-17) ); 
         assertTrue(  equalsApprox(1.000000000002e-00,  1e-00) );
         assertTrue(  equalsApprox(1.000000000002e+17,  1e+17) );
@@ -124,8 +158,6 @@ class UtilsIan {
         assertFalse( equalsApprox(1.000000200002e-00,  1e-00) ); 
         assertFalse( equalsApprox(1.000000200002e+17,  1e+17) ); 
 
-        assertTrue(  equalsApprox(0.0,  1e-320, 11) ); // passes
-        assertTrue(  equalsApprox(0.0,  1e-321, 10) ); // passes
         }
 
     /** test equalsApprox(x,y) as well as (y,x) and (1/x,1/y) and (1/y,1/x). */
@@ -186,10 +218,13 @@ class UtilsIan {
             }
         }
     public static void logFailedAssertEquals(Object a, Object b, boolean expectEqual) {
-        System.out.printf("assert failed: %s %s %s\n", a,  expectEqual?"==":"!=", b);
+        log("assert failed: %s %s %s\n", a,  expectEqual?"==":"!=", b);
         }
 
     public static void logPassedAssertEquals(Object a, Object b, boolean expectEqual) {
+        log(".");
+        }
+    public static void log(String msg) {
         System.out.printf("." );
         }
 
